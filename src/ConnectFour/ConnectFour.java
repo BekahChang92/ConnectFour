12b388e119f7ca4d564b3077e6753fcae65efd14
REBEKAH <rebekahchang92@gmail.com>
2016-11-13 01:43:56 -0500

    implemented actionlistener to connect4 and added action listener for all buttons

--- src/ConnectFour/ConnectFour.java
+++ src/ConnectFour/ConnectFour.java
@@ -9,25 +9,25 @@ import java.util.Scanner;
  *
  * @author rebekah
  */
-public class ConnectFour extends JFrame {
+public class ConnectFour extends JFrame implements ActionListener {
     
     private Container contents;
     private JButton[][] connect4board;
+    private JPanel welcome, gameBoard;
     private char[][] board;
-    private String player1;
-    private String player2;
+    private JButton startbutton;
     public ConnectFour(char[][] board){
         super("Connect Four");
-        this.player1 = player1;
-        this.player2 = player2;
         this.board = board;
         contents = getContentPane();
+        ButtonHandler bh = new ButtonHandler();
         //welcome screen or panel
-        JPanel welcome = new JPanel();
+        welcome = new JPanel();
         welcome.setLayout(new BoxLayout(welcome, BoxLayout.PAGE_AXIS));
         JLabel welcometext = new JLabel("Let's play Connect 4!");
         JLabel startText = new JLabel("Press 'Start' to being the game!");
         JButton startbutton = new JButton("Start");
+        startbutton.addActionListener(bh);
         welcome.add(welcometext);
         welcome.add(startText);
         welcome.add(startbutton);
@@ -43,8 +43,13 @@ public class ConnectFour extends JFrame {
         JPanel panel3 = new JPanel();
         panel3.setSize(500, 750);
         */
-        JLabel player1name = new JLabel(this.player1);
         
+    }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == startbutton){
+            gameBoard = new JPanel( new GridLayout());
+            
+        }
     }
     public JButton[][] getBoard(){
         for(int i = 0; i < board[0].length; i++) {
@@ -67,5 +72,4 @@ public class ConnectFour extends JFrame {
         ConnectFour connectFour = new ConnectFour(board);
     }
 }
-    
 
